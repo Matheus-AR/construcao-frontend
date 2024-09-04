1d5a9fd2951c8346e7c78949ea0bfa900312d1de
Matheus de Araujo Rodrigues <matheusaraujorodrigues@outlook.com>
2024-09-03 21:49:41 -0300

    Aula 5

--- aulas/aula04/src/components/InputUsuario.jsx
+++ aulas/aula04/src/components/InputUsuario.jsx
@@ -1,6 +1,9 @@
 function InputUsuario() {
     return (
-        <></>
+        <>
+            <label htmlFor="usuario">Nome</label>
+            <input type="email" id="usuario" name="usuario" required />
+        </>
     )
 }
 
--- aulas/aula04/src/components/Logo.jsx
+++ aulas/aula04/src/components/Logo.jsx
@@ -1,10 +1,7 @@
-import './Logo.css'
+import "./Logo.css";
 
 function Logo(props) {
-    const texto = "Logo da Aplicação"
-    return (
-        <img className="logo" src={props.imagem} alt={texto} />
-    )
+  return <img className="logo" src={props.imagem} alt={props.texto} />;
 }
 
-export default Logo
+export default Logo;
