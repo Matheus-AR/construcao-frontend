cb0356a205d4ee2b7de11da54a77b965b6cd5c71
Matheus de Araujo Rodrigues <matheusaraujorodrigues@outlook.com>
2024-09-24 21:45:18 -0300

    Aula 6

---
+++ aulas/aula06/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Perfil from "./pages/Perfil";
+import Error404 from "./pages/Error404";
+import Leiaute from "./pages/Leiaute";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route element={<Leiaute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
